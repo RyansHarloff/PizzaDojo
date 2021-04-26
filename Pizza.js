b519d73a178fac0254d231263ff71550fe9ac7d7
Ryan Harloff <ryans.harloff@gmail.com>
2021-04-26 12:54:22 -0500

    DojoPizza

---
+++ Pizza.js
@@ -0,0 +1,47 @@
+function pizzaOven(crust, sauce, cheeses, toppings) {
+    var pizza = {};
+    pizza.crust = crust;
+    pizza.sauce = sauce;
+    pizza.cheese = cheeses;
+    pizza.toppings = toppings;
+    return pizza;
+}
+    
+var pizza1 = pizzaOven("deepdish", "traditional", "mozarella", ["sausage", "pepperoni"]);
+console.log(pizza1);
+
+function pizzaOven(crust, sauce, cheeses, toppings) {
+    var pizza = {};
+    pizza.crust = crust;
+    pizza.sauce = sauce;
+    pizza.cheese = cheeses;
+    pizza.toppings = toppings;
+    return pizza;
+}
+    
+var pizza2 = pizzaOven("handtossed", "marinara", ["mozarella","feta"], ["mushrooms", "olives","onions"]);
+console.log(pizza2);
+
+function pizzaOven(crust, sauce, cheeses, toppings) {
+    var pizza = {};
+    pizza.crust = crust;
+    pizza.sauce = sauce;
+    pizza.cheese = cheeses;
+    pizza.toppings = toppings;
+    return pizza;
+}
+    
+var pizza3 = pizzaOven("pan", "traditional", "mozarella", ["extra cheese", "sausage","bacon"]);
+console.log(pizza3);
+
+function pizzaOven(crust, sauce, cheeses, toppings) {
+    var pizza = {};
+    pizza.crust = crust;
+    pizza.sauce = sauce;
+    pizza.cheese = cheeses;
+    pizza.toppings = toppings;
+    return pizza;
+}
+    
+var pizza3 = pizzaOven("thin crust", "traditional", "mozarella", "extra pepperoni");
+console.log(pizza4);
